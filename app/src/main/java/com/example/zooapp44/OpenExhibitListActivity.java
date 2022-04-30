f47c6575a16a9fb52e24d3154b729951b887d4da
Rorshachk <shuangmuhu@gmail.com>
2022-04-30 01:14:35 -0700

    add two buttons

---
+++ app/src/main/java/com/example/zooapp44/OpenExhibitListActivity.java
@@ -0,0 +1,14 @@
+package com.example.zooapp44;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OpenExhibitListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_exhibit_list);
+    }
+}
