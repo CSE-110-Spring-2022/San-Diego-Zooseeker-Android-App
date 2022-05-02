1968e44a2a95f3794c2b87ce4a3062d6eb797317
Rorshachk <shuangmuhu@gmail.com>
2022-05-01 18:13:34 -0700

    test passed, feature done

--- app/src/androidTest/java/com/example/zooapp44/OpenExhibitListActivityTest.java
+++ app/src/androidTest/java/com/example/zooapp44/OpenExhibitListActivityTest.java
@@ -4,10 +4,18 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import androidx.lifecycle.Lifecycle;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -27,19 +35,45 @@ public class OpenExhibitListActivityTest {
     // I am still confused about
     // how to implement a test for putting extras
 
-    static Intent intent;
-    static {
-        intent = new Intent(ApplicationProvider.getApplicationContext(), OpenExhibitListActivity.class);
-        List<String> exhibits = Arrays.asList("Tiger", "Bear", "Dog", "Elephant");
-        List<String> distance = Arrays.asList("300ft", "500ft", "200ft", "100ft");
-        ExhibitRoute route = new ExhibitRoute(exhibits, distance);
-    }
+//    static Intent intent;
+//    static {
+//        intent = new Intent(ApplicationProvider.getApplicationContext(), OpenExhibitListActivity.class);
+//        List<String> exhibits = Arrays.asList("Tiger", "Bear", "Dog", "Elephant");
+//        List<String> distance = Arrays.asList("300ft", "500ft", "200ft", "100ft");
+//        ExhibitRoute route = new ExhibitRoute(exhibits, distance);
+//    }
+//
+//    @Rule
+//    public ActivityScenarioRule<OpenExhibitListActivity> activityScenarioRule = new ActivityScenarioRule<>(intent);
 
-    @Rule
-    public ActivityScenarioRule<OpenExhibitListActivity> activityScenarioRule = new ActivityScenarioRule<>(intent);
+//    @Rule
+//    public IntentsTestRule<OpenExhibitListActivity> intentsTestRule = new IntentsTestRule<>(OpenExhibitListActivity.class);
 
     @Test
     public void testOpenExhibitListActivityNoneEmptyList(){
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), OpenExhibitListActivity.class);
+        List<String> exhibits = Arrays.asList("Tiger", "Bear", "Dog", "Elephant");
+        List<String> distance = Arrays.asList("300ft", "500ft", "200ft", "100ft");
+        intent.putExtra("Route", ExhibitRoute.serialize(new ExhibitRoute(exhibits, distance)));
+
+        ActivityScenario<OpenExhibitListActivity> scenario = ActivityScenario.launch(intent);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+        scenario.moveToState(Lifecycle.State.RESUMED);
+
+        scenario.onActivity(activity -> {
+            RecyclerView recyclerView = activity.recyclerView;
+            for(int i = 0; i < exhibits.size(); i++){
+                RecyclerView.ViewHolder viewHolder = recyclerView.findViewHolderForAdapterPosition(i);
+                assertNotNull(viewHolder);
+
+                TextView animalNameTV = viewHolder.itemView.findViewById(R.id.animal_name);
+                TextView distanceTV = viewHolder.itemView.findViewById(R.id.distance);
+
+                assertEquals(animalNameTV.getText().toString(), exhibits.get(i));
+                assertEquals(distanceTV.getText().toString(), distance.get(i));
+            }
+        });
     }
 
 }
