65b379f94711f8cc1d8c51387a2eec9b95b3e4cc
HaitongC <haitongchen116@gmail.com>
2022-05-05 21:18:19 -0700

    added GetDirectionActivity class

---
+++ app/src/main/java/com/example/zooapp44/GetDirectionActivity.java
@@ -0,0 +1,16 @@
+package com.example.zooapp44;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GetDirectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+    }
+
+
+}
