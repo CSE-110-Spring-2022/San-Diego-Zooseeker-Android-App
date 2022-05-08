02a45f1c6e0728d33d6ab0fa486a7641d21147cc
HaitongC <haitongchen116@gmail.com>
2022-05-08 08:25:03 -0700

    modify "HOME" button's functionality
    HOME button now takes you from navigation page(get direction activity) to home page(main activity)

--- app/src/main/java/com/example/zooapp44/GetDirectionActivity.java
+++ app/src/main/java/com/example/zooapp44/GetDirectionActivity.java
@@ -1,6 +1,7 @@
 package com.example.zooapp44;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -49,6 +50,8 @@ public class GetDirectionActivity extends AppCompatActivity {
     }
 
     public void onHomeClicked(View view){
+        super.onBackPressed();
+        startActivity(new Intent(GetDirectionActivity.this, MainActivity.class));
         finish();
     }
 
