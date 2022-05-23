9d470bdd5a05aac439831d793caa80a9f4fc9894
Rorshachk <shuangmuhu@gmail.com>
2022-05-22 23:03:19 -0700

    make instruction display scrollable

--- app/src/main/java/com/example/zooapp44/GetDirectionActivity.java
+++ app/src/main/java/com/example/zooapp44/GetDirectionActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -49,6 +50,7 @@ public class GetDirectionActivity extends AppCompatActivity {
 
 
         TextView instructionView = findViewById(R.id.route_instruction);
+        instructionView.setMovementMethod(new ScrollingMovementMethod());
         instructionView.setText(route.getInstruction(current));
 
         updateNextAnimalView();
