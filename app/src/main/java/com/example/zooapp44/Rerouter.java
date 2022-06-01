601bed63822c4dc4dd5175f25b70e495eead3712
Rorshachk <shuangmuhu@gmail.com>
2022-05-31 23:06:43 -0700

    reroute done

--- app/src/main/java/com/example/zooapp44/Rerouter.java
+++ app/src/main/java/com/example/zooapp44/Rerouter.java
@@ -14,13 +14,17 @@ public class Rerouter {
 
         double min_d = 1e9;
         String nearestVertex = "";
+        ZooGraph zooGraph = ZooGraph.getSingleton(context);
         for(Map.Entry<String, Coord> entry:vertexCoord.entrySet()){
             if(Coord.calcDistance(entry.getValue(), coord) < min_d){
                 nearestVertex = entry.getKey();
+                min_d = Coord.calcDistance(entry.getValue(), coord);
+                if(zooGraph.vInfo.get(nearestVertex).group_id != null)
+                    nearestVertex = zooGraph.vInfo.get(nearestVertex).group_id;
             }
         }
 
-        ZooGraph zooGraph = ZooGraph.getSingleton(context);
+
         ExhibitRoute newRoute = zooGraph.getOptimalPath(nearestVertex, unvisited);
         newRoute.current_coord = vertexCoord.get(nearestVertex);
 
