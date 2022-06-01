a0454d7bb6339d7b7419aae82c569d43922e2a2a
Stella <l9zhang@ucsd.edu>
2022-05-31 18:50:31 -0700

    fixed minor errors

--- app/src/main/java/com/example/zooapp44/MainActivity.java
+++ app/src/main/java/com/example/zooapp44/MainActivity.java
@@ -1,5 +1,14 @@
 package com.example.zooapp44;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
